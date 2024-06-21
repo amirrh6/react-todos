6434de9bd1f21f1477eda1e0f8431bf8bac382ef
amirrh6 <amirrezahh6@gmail.com>
2024-06-21 17:50:47 +0330

    Added ViewAllTODOs component

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,7 @@ const App = () => {
         <>
             <Navbar />
             <Items />
-
-            <section className="m-auto max-w-lg my-10 px-6">
-                <a
-                    href="#"
-                    className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-                >
-                    View All TODOs
-                </a>
-            </section>
+            <ViewAllTODOs />
         </>
     );
 };
---
+++ src/components/ViewAllTODOs.jsx
@@ -0,0 +1,14 @@
+const ViewAllTODOs = () => {
+    return (
+        <section className="m-auto max-w-lg my-10 px-6">
+            <a
+                href="#"
+                className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+            >
+                View All TODOs
+            </a>
+        </section>
+    );
+};
+
+export default ViewAllTODOs;

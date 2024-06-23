0f4e864d417f6e246f817fd4ab132b51ef36eb3c
amirrh6 <amirrezahh6@gmail.com>
2024-06-23 23:02:51 +0330

    Minor

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -11,7 +11,7 @@ const Items = ({ returnRecentOnly = false }) => {
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Most recent TODOs
+                    {returnRecentOnly ? 'Most recent TODOs' : 'Browse TODOs'}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {TODOsList.map((todo) => (

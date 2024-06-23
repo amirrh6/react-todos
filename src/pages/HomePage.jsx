ecc69b29c79434c800cd950ad25e8cbf8184677b
amirrh6 <amirrezahh6@gmail.com>
2024-06-23 21:54:22 +0330

    Use of react-router-dom's Outlet
    to render a layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import Navbar from '../components/Navbar';
 import Items from '../components/Items';
 import ViewAllTODOs from '../components/ViewAllTODOs';
 
 const HomePage = () => {
     return (
         <>
-            <Navbar />
             <Items />
             <ViewAllTODOs />
         </>

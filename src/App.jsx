c5438f631435d9095d064ffb75621e630dec63f9
amirrh6 <amirrezahh6@gmail.com>
2024-06-23 22:14:27 +0330

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import TODOsPage from './pages/TODOsPage';
 import AddTODOPage from './pages/AddTODOPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
             <Route index element={<HomePage />} />
             <Route path="/todos" element={<TODOsPage />} />
             <Route path="/add-todo" element={<AddTODOPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Route>
     )
 );

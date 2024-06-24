e0083cb1d243e3a4d49c407b38c5b21fc8cd4ab6
amirrh6 <amirrezahh6@gmail.com>
2024-06-24 12:16:42 +0330

    Check EXPOSE_TO_NETWORK environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+    plugins: [react()],
+    server: {
+        port: 3000,
+        // eslint-disable-next-line no-undef
+        host: process.env.EXPOSE_TO_NETWORK !== undefined ? '0.0.0.0' : '127.0.0.1',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
-  }
-})
+});

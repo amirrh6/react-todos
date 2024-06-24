f8ef3c70a51ab59d504a85aa6f7b48eac6e4f8ba
amirrh6 <amirrezahh6@gmail.com>
2024-06-24 15:08:48 +0330

    Minor

--- src/pages/TODOPage.jsx
+++ src/pages/TODOPage.jsx
@@ -59,7 +59,7 @@ const TODOPage = () => {
                             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                                 <h3 className="text-xl font-bold mb-6">Manage TODO</h3>
                                 <Link
-                                    to="#"
+                                    to={`/todos/edit/${todo.id}`}
                                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                                     onClick={(event) => {
                                         event.preventDefault();
